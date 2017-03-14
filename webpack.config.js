80c2e6ce22c1d4206abfa1a3b7c59bb952fd6881
Bret <Burhooo@gmail.com>
2017-03-14 15:44:40 -0500

    getting git set up

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require("path");
+
+module.exports = {
+	entry: './src/app.js',
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: 'app.bundle.js'
+	},
+	module: {
+		rules: [
+				{
+       			test: /\.css$/,
+        		use: ExtractTextPlugin.extract({
+          			fallback: "style-loader",
+          			use: "css-loader",
+          			publicPath: '/dist'
+        			})
+     			}
+				]
+	},
+	devServer: {
+		  contentBase: path.join(__dirname, "dist"),
+		  compress: true,
+		  port: 8080,
+		  open: true
+	},
+	plugins: [new HtmlWebpackPlugin({
+    	title: 'Wikipedia Viewer',
+    	minify: {
+    		collapseWhitespace: true
+    	},
+    	template: './src/index.ejs', // Load a custom template (ejs by default see the FAQ for details)
+  	}),
+	new ExtractTextPlugin({
+    	filename: "bundle.css",
+    	disable: false,
+    	allChunks: true
+  	})
+	]
+}
