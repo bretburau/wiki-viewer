902e1c015faf07a6fb5fcf21a5500174ae18c715
Bret <Burhooo@gmail.com>
2017-03-21 07:20:11 -0500

    Working on getting AJAX and CORS to work

--- src/app.js
+++ src/app.js
@@ -1,3 +1,36 @@
-const css = require('./app.css');
+const css = require('./app.css'); //from external css module of webpack
 
-console.log("hello from app.js and webserver cool huh");
+
+function search(e) {
+	e.preventDefault();
+	//build the query string
+	var url = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=jsonp&srsearch=steve+Gadd";
+	var searchText = document.getElementById("search").value;
+	console.log(searchText);
+	var xhr = new XMLHttpRequest();
+    if (!('withCredentials' in xhr)) xhr = new XDomainRequest(); // fix IE8/9
+    xhr.open('GET', url);
+    xhr.onreadystatechange = function() { //callback
+    	if (this.readyState == 4 && this.status == 200) { //ensure ajax worked
+
+
+    	} //else handle errors
+
+    } //end callback
+    xhr.send();
+	/*
+	alert(searchText);
+	//make the ajax call
+	var httpRequest = new XMLHttpRequest;
+	httpRequest.onreadystatechange = function() { ///ajax callback
+		//process the json 
+	}// end onreadystatechange
+
+	httpRequest.open('GET', url, true);
+	httpRequest.send(null);
+	*/
+} //end search function
+
+
+document.getElementById("searchButton").onclick = search;
+
