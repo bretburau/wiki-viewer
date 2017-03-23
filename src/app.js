e695c03a0f6a418317252781152bcbbfbf86ad02
Bret <Burhooo@gmail.com>
2017-03-23 07:03:37 -0500

    Got ajax CORS working

--- src/app.js
+++ src/app.js
@@ -1,34 +1,26 @@
 const css = require('./app.css'); //from external css module of webpack
 
 
+
+//var url = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=jsonp&srsearch=steve+Gadd&origin=*";
+
 function search(e) {
-	e.preventDefault();
-	//build the query string
-	var url = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=jsonp&srsearch=steve+Gadd";
-	var searchText = document.getElementById("search").value;
-	console.log(searchText);
-	var xhr = new XMLHttpRequest();
-    if (!('withCredentials' in xhr)) xhr = new XDomainRequest(); // fix IE8/9
-    xhr.open('GET', url);
-    xhr.onreadystatechange = function() { //callback
-    	if (this.readyState == 4 && this.status == 200) { //ensure ajax worked
+	e.preventDefault(); //stop page from reloading
 
-
-    	} //else handle errors
-
-    } //end callback
-    xhr.send();
-	/*
-	alert(searchText);
-	//make the ajax call
-	var httpRequest = new XMLHttpRequest;
-	httpRequest.onreadystatechange = function() { ///ajax callback
-		//process the json 
-	}// end onreadystatechange
-
-	httpRequest.open('GET', url, true);
-	httpRequest.send(null);
-	*/
+	 $.ajax({
+	 	url: "https://en.wikipedia.org/w/api.php",
+	 	data: {
+	 		action: "query",
+	 		format: "json",
+	 		list: "search",
+	 		srsearch: "Steve Gadd",
+	 		origin: "*"
+	 	},
+	 	success: function(json) {
+	 		alert(json);
+	 	}
+	 }); // end .ajax
+	 	
 } //end search function
 
 
